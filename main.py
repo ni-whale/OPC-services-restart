c5bb3eb3d3993cd7f0e4048946ac064993a9c1f4
ni_whale <1nibob1@gmail.com>
2021-09-27 18:53:08 +0300

    Test build ver. 1.0. It was tested on the production server and executed without issues.

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def services_restart(connection, service_name):
 def remote_connection():
     global remote_pc, SERVICES
     try:
-        connection = wmi.WMI(remote_pc, user=r"", password="")
+        connection = wmi.WMI(remote_pc, user=r"pcg\svcbackups", password="pcgbackups")
         logging.info(f"[+] The user '{current_user()}' initiated services reboot. The reason: '{e_comment.get()}'.")
     except:
         logging.error(f"[!] Couldn't connect to the server {remote_pc}.")
